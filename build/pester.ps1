bd102d1652484b9b09a9f2b6f63e3abdfb979519
Steviecoaster <stephen@chocolatey.io>
2019-07-26 15:40:42 +0000

    Move Keeper and add unit tests.

---
+++ build/pester.ps1
@@ -0,0 +1,10 @@
+If(!(Get-Module Pester)){
+    Install-Module -Name Pester -SkipPublisherCheck -Force
+}
+
+Import-Module Pester
+
+. ./Get-ChocolateyPackageMetaData.ps1
+
+Invoke-Pester ../tests/package.tests.ps1
+
