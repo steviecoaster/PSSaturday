4a9aaf08e10190c6ddc9bda1a9b2c640a0111630
Steviecoaster <stephen@chocolatey.io>
2019-07-26 16:36:32 +0000

    Verifying path

--- build/pester.ps1
+++ build/pester.ps1
@@ -2,5 +2,9 @@ If(!(Get-Module Pester)){
     Install-Module -Name Pester -SkipPublisherCheck -Force
 }
 
+echo "I'm $env:Build.ArtifactStagingDirectory"
+echo "I'm: $($Build.ArtifactStagingDirectory)"
+echo "I'm: $env:Build_ArtifactStagingDirectory"
+
 Invoke-Pester ./tests/package.tests.ps1 -OutputFile "$($env:Build.ArtifactStagingDirectory)\$($env:Package).xml" -OutputFormat 'NUnitXml'
 
