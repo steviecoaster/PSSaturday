a41fae14e77607b15a2124fa9a67b06e91a43c96
Steviecoaster <stephen@chocolatey.io>
2019-07-26 16:38:04 +0000

    Figured path out I think

--- build/pester.ps1
+++ build/pester.ps1
@@ -2,9 +2,5 @@ If(!(Get-Module Pester)){
     Install-Module -Name Pester -SkipPublisherCheck -Force
 }
 
-echo "I'm $env:Build.ArtifactStagingDirectory"
-echo "I'm: $($Build.ArtifactStagingDirectory)"
-echo "I'm: $env:Build_ArtifactStagingDirectory"
-
-Invoke-Pester ./tests/package.tests.ps1 -OutputFile "$($env:Build.ArtifactStagingDirectory)\$($env:Package).xml" -OutputFormat 'NUnitXml'
+Invoke-Pester ./tests/package.tests.ps1 -OutputFile "$($env:Build_ArtifactStagingDirectory)\$($env:Package).xml" -OutputFormat 'NUnitXml'
 
