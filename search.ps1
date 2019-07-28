8b4e6d86a2c7a160ff59081ea59dff423a54cf97
Steviecoaster <stephen@chocolatey.io>
2019-07-28 01:30:00 +0000

    [bill] add versbosity to search script for debug

--- search.ps1
+++ search.ps1
@@ -1,22 +1,21 @@
 $changes = gci $pwd -Recurse -Include '*.nuspec' | Where-Object { $_.Name -eq $env:ChangedNuspec }
-      
-      If($changes.Count -gt 0) {
-      $nuspecLocation =  ($changes | ? { $_.Extension -eq '.nuspec' -and $_.Directory}).DirectoryName
-      $filter =  gci -Path $nuspecLocation -Filter '*.nuspec' | Select-Object FullName, BaseName,Name
-      $nuspec = $filter.Fullname
-      echo "Nuspec path: $nuspec"
-      $packageName = $filter.Basename
-      echo "Package name: $packageName"
-      $artifact = $filter.Name
-      echo $artifact
-      echo "##vso[task.setvariable variable=NUSPECLOCATION]$nuspecLocation"
-      echo "##vso[task.setvariable variable=NUSPECNAME]$nuspec"
-      echo "##vso[task.setvariable variable=ArtifactName]$artifact"
-      echo "##vso[task.setvariable variable=Package]$packageName"
-      
-      }
-      
-      else { 
-      
-      throw "No suitable changes detected to build chocolatey package"
-      }
+
+$changes
+
+$nuspecLocation =  ($changes | ? { $_.Extension -eq '.nuspec' -and $_.Directory}).DirectoryName
+$filter =  gci -Path $nuspecLocation -Filter '*.nuspec' | Select-Object FullName, BaseName,Name
+
+$nuspec = $filter.Fullname
+echo "Nuspec path: $nuspec"
+
+$packageName = $filter.Basename
+echo "Package name: $packageName"
+
+$artifact = $filter.Name
+echo $artifact
+
+
+echo "##vso[task.setvariable variable=NUSPECLOCATION]$nuspecLocation"
+echo "##vso[task.setvariable variable=NUSPECNAME]$nuspec"
+echo "##vso[task.setvariable variable=ArtifactName]$artifact"
+echo "##vso[task.setvariable variable=Package]$packageName"
